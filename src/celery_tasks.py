6af76eecba291c2bdcfc216f58789a699d8b3a29
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-03-03 16:03:50 +0530

    relaced background tasks with celery

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 from asgiref.sync import async_to_sync
 from typing import List
 from src.mail import send_email
+import logging
 
 
 
@@ -10,12 +11,12 @@ c_app=Celery()
 c_app.config_from_object("src.config")
 
 @c_app.task()
-def send_email_tasks(self,recipients:List[str],subject:str,body:str):
+def send_email_tasks(recipients:List[str],subject:str,body:str):
     try:
         send_email(recipients, subject, body)
         return "Email sent successfully"
     except Exception as e:
-        raise self.retry(exc=e, countdown=5)
+        logging.error(str(e))
     
 
 
