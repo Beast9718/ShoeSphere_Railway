b9bef34dcb3f40a0f82e42a977f8a5959e5887f3
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-03-04 23:40:34 +0530

    ..

--- src/db/main.py
+++ src/db/main.py
@@ -1,14 +1,15 @@
 from sqlmodel import create_engine,text ,SQLModel
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine
 from src.config import Config
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
-async_engine=AsyncEngine(
-    create_engine(
+async_engine=create_async_engine(
     url=Config.DATABASE_URL,
-    echo=True
+    echo=True,
+    pool_pre_ping=True
+    
 )
-)
+
 
 async def init_db():
     async with async_engine.begin() as connection:
